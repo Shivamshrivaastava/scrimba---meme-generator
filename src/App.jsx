9a2c0d21682393f3cac21e7cc2c4c8e5da1f50f1
shivam shrivastava <shivamshrivaastava@gmail.com>
2024-04-17 20:22:53 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import './App.css';
+import Meme from "./components/Meme";
+
+const App = ( ) => {
+	return (
+		<div className="App">
+			<Header />
+			<Meme />
+		</div>
+	);
+}
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header className="header">
+        <img src="public\troll-face.png"
+        className="header--image" 
+        />
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">Scrimba-Project</h4>
+        </header>
+    );
+}
+
+export default Header
